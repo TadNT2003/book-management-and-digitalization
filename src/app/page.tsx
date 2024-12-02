cb494523f634ec684c4e555369e64bc2b3a36da7
Nguyen Thanh Dat <ntdatwows2003@gmail.com>
2024-12-02 14:51:46 +0700

    After copy: not compatible yet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,48 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
+"use client"
+import BookCard from '@/components/BookCard'
+import Header from '@/components/Header'
+import SideBar from '@/components/SideBar'
+import { books } from '@/constants/mockData'
+import { motion } from 'framer-motion'
+import styles from './page.module.css'
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className={styles.main}>
+  <div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+    <Header/>
+
+      <div className={styles.containerStyle}>
+    <section className={styles.content}>
+<SideBar/>
+    </section>
+
+<div className={styles.grouper}>
+  <h1 className={styles.title}>ALL BOOKS</h1>
+  <ul className={styles.ulGroupStyle}>
+    {
+      books.map((book,i)=>
+        <motion.li 
+        whileHover={{scale:1.1}}
+        whileTap={{scale:0.9}}
+        transition={{type:'spring',damping:50,mass:0.75}}
+        initial={{opacity:0,x:200*(i+1)}}
+        animate={{opacity:1,x:0}}
+        key={i}>
+          <a href={`/book/${book.id}`} style={{textDecoration:'none'}} >
+           <BookCard title={book.title} coverImage={book.image} description={book.description} />
           </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        </motion.li>
+      )
+    }
+  </ul>
+</div>
+
+      </div>
+
+  </div>
+    </main>
+  )
 }
+
+
